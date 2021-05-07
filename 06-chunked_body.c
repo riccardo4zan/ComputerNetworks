6b7ff9a49fba20b717f9cc6a28973ec0cd94825f
riccardoforzan <forzanric@gmail.com>
2021-05-07 19:01:38 +0200

    Terminate chunked_body

--- 06-chunked_body.c
+++ 06-chunked_body.c
@@ -11,7 +11,8 @@
  * man read
  * man 2 write
  * man printf
- * man atol
+ * man bzero
+ * man strtol
  */
 
 #include <stdlib.h>
@@ -23,6 +24,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_LENGTH 10000
+
 struct header{
     char *n;
     char *v;
@@ -31,7 +34,7 @@ struct header{
 int main(){
 
     struct sockaddr_in addr;
-    char request[5000], response[10000];
+    char request[5000]; 
     unsigned char targetip[4] = {216, 58, 213, 100};
 
     //This variable contains the number of the socket opened by the SO
@@ -58,22 +61,26 @@ int main(){
     //This array of struct stores the headers (100 at most)
     struct header h[100];
 
-    char *statusline = h[0].n = response;
+    //Buffer is used to parse the response    
+    char* buffer;
+    buffer = (char*)malloc(MAX_LENGTH);
+
+    char *statusline = h[0].n = buffer;
 
     //This variable stores the number of headers 
     int k=0;
 
-    for (int j = 0; read(s, response + j, 1); j++){
-        if (response[j] == ':' && (h[k].v == 0)){
-            response[j] = 0;
+    for (int i = 0; read(s, buffer + i, 1); i++){
+        if (buffer[i] == ':' && (h[k].v == 0)){
+            buffer[i] = 0;
             //Trim the first space
-            h[k].v = response + j + 2;
+            h[k].v = buffer + i + 2;
         }
-        else if ((response[j] == '\n') && (response[j - 1] == '\r')){
-            response[j - 1] = 0;
+        else if ((buffer[i] == '\n') && (buffer[i - 1] == '\r')){
+            buffer[i - 1] = 0;
             if (h[k].n[0] == 0)
                 break;
-            h[++k].n = response + j + 1;
+            h[++k].n = buffer + i + 1;
         }
 
     }
@@ -90,12 +97,64 @@ int main(){
         printf("%s ----> %s\n", h[i].n, h[i].v);
     }
 
+    //Additions
+
+    free(buffer);
+    buffer=(char*)malloc(50);
+
     /**
-     * TODO:
-     * Leggere la lunghezza in esadecimale del chunked body
-     * Creare un buffer della lunghezza del chunked body
-     * Leggere il numero di bytes indicato dalla lunghezza
-     * Finchè non vengono date altre lunghezze per il chunked body
-     */ 
+     * Reading the first line of the chunked body, until CRLF is found
+     * is possible that the chunked body contains chunked header but we are
+     * not intrested in it. It's guaranteed by the grammar that the first number 
+     * is the exadecimal rappresentation of how long the chunked body is.
+    */
+
+    unsigned int total_body_length=1;
+    char* entity_body = (char*)malloc(1);
+
+    unsigned int chunk_length;
+
+    //This is a temporary buffer to store the exadecimal value of chunk length
+    bzero(buffer,50);
+    char* tmp = buffer;
+
+    //Reading chunk length
+    for(int i=0;read(s,buffer+i,1);i++){
+        if((buffer[i]=='\n') && (buffer[i-1]=='\r')){
+            buffer[i-1]=0;
+            break;
+        }
+    }
+    chunk_length = strtol(tmp,NULL,16);
+    total_body_length += chunk_length;
+    printf("Chunk length: %d\n",chunk_length);
+
+    while(chunk_length>0){ 
+        //Allocating the space and reading entity body
+        entity_body = (char*)realloc(entity_body,total_body_length); 
+        read(s,entity_body,chunk_length);
+       
+       	/*
+        //Look for another chunk length
+        bzero(buffer,50);
+        int t;
+        for(int i=0;t = read(s,buffer+i,1);i++){     
+            printf("%d",t);
+             if((buffer[i]=='\n') && (buffer[i-1]=='\r')){
+                buffer[i-1]=0;
+            }
+        }
+        if(t>0){
+            chunk_length = strtol(tmp,NULL,16);
+            total_body_length += chunk_length;
+            printf("Chunk length: %d\n",chunk_length);
+        } else*/ 
+            break;
+    }
+
+    
+    //Terminate the entity body and print it out
+    entity_body[total_body_length]=0;
+    printf("%s",entity_body);
 
 }
